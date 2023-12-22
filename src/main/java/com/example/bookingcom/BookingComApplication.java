c671cb1765ca21e130b1de325e8c47b5c715e092
mavjudbek <mavjudbeksokhibov@gmail.com>
2023-12-22 20:16:38 +0500

    commit

---
+++ src/main/java/com/example/bookingcom/BookingComApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingComApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingComApplication.class, args);
+    }
+
+}
